1331dabd2b36123f1488e66cb108aedcceb34041
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-09 16:43:16 +0100

    Develop the logic to start the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
+const fixCards = [
+  { src: "./public/img/helmet.png" },
+  { src: "./public/img/potion.png" },
+  { src: "./public/img/ring.png" },
+  { src: "./public/img/scroll.png" },
+  { src: "./public/img/shield.png" },
+  { src: "./public/img/sword.png" },
+];
+
 function App() {
+  const [turns, setTurns] = useState(0);
+  const [cards, setCards] = useState([]);
+
+  const startGame = () => {
+    const shuffledCards = [...fixCards, ...fixCards]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => {
+        return { ...card, id: Math.random() * 10 };
+      });
+
+    console.log("Cards are -->", shuffledCards);
+    setCards(shuffledCards);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={startGame}>New Game</button>
     </div>
   );
 }
